685d59485b6c554e6cbfa9e8d012a41d8871ba90
Anoop Rajendra <anoop.rajendra@gmail.com>
2020-05-07 12:55:25 -0700

    Package Update

--- bootup/src/pages/index.js
+++ bootup/src/pages/index.js
@@ -8,13 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>StartUp Sequence</h1>
+    <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-	<a href="resume.html">Resume</a>
+    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
